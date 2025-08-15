3fcb61adb49d2eda95238cf746aead8a44f7cc33
Emmanuel Omejua <omejuaifeanyi2@mail.com>
2025-08-15 19:22:17 +0100

    little changes in chart section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],       
+  weight: ['300', "400", "500", '600', "700"], 
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "FundR",
@@ -27,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         <div className="max-w-[1440px] w-full h-full flex flex-col">
 
